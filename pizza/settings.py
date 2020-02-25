00587357af4a82f7a68f25198b2e96f50bb95cd8
yannis <gnikolaidhs95@gmail.com>
2020-02-25 03:21:38 -0800

    fixed bug

--- pizza/settings.py
+++ pizza/settings.py
@@ -138,3 +138,6 @@ STATIC_URL = 'orders/static/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
